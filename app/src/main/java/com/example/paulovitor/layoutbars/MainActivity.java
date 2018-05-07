69d6950732ebbaccab955134b9a2d5b37fc55fdf
paulo9mv <paulo9mv@yahoo.com.br>
2018-05-07 16:59:56 -0400

    Adicionado menu

--- app/src/main/java/com/example/paulovitor/layoutbars/MainActivity.java
+++ app/src/main/java/com/example/paulovitor/layoutbars/MainActivity.java
@@ -2,12 +2,68 @@ package com.example.paulovitor.layoutbars;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends DebugActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        //Infla o menu com o que está no XML
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(onSearch());
+
+        return true;
+    }
+
+    private SearchView.OnQueryTextListener onSearch(){
+        return new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, "Submetido", Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Toast.makeText(MainActivity.this, "Mudado!", Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+        };
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.action_search){
+            toast("Search!");
+        }
+        else if(id == R.id.action_refresh){
+            toast("Refresh!");
+        }
+        else if(id == R.id.action_settings){
+            toast("Settings!");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void toast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
 }
